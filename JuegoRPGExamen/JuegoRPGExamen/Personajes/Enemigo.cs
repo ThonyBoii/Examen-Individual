4e853e08579c35694b38562b88a40231ca423143
ThonyBoii <thonyabarca1705@gmail.com>
2024-09-20 17:03:40 -0500

    creation of classes and adding attributes to the Enemy and Character classes

---
+++ JuegoRPGExamen/JuegoRPGExamen/Personajes/Enemigo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoRPGExamen.Personajes
+{
+    internal class Enemigo
+    {
+        public string NombreE { get; set; }
+        public int DefenzaE { get; set; }
+        public int VidaE { get; set; }
+        public int VelocidadE { get; set; }
+    }
+}
---
+++ JuegoRPGExamen/JuegoRPGExamen/Personajes/Personaje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoRPGExamen.Personajes
+{
+    internal class Personaje
+    {
+        public string Nombre { get; set; }
+        public int Defenza { get; set; }
+        public int Vida { get; set; }
+        public int Velocidad { get; set; }
+    }
+}
